ad37339352473066f7a4ef90b685534978ddeb61
lukas <lukas@grunwald-elzach.de>
2021-12-15 23:37:47 +0100

    2.3 main acctions to separate file

--- main.py
+++ main.py
@@ -12,60 +12,8 @@ import configure_massage
 import smtp_interactions
 import imap_interactions
 import triggers
-import accessories
 
-
-def RunAllMails():
-    '''
-    Runs all necessary functions to build and send mails to all persons with
-    pending payments.
-    '''
-    log_writing.ProtSys('start triggered operation')
-    # get data on the people in the spredsheet and removing thos without dept.
-    data = obtain_data.DelNonDebtor(obtain_data.ReadTable())
-    log_writing.ProtSys('read data from spreadsheet')
-    # define vaiable for all mails
-    N = len(data)
-    mails = [0] * N
-    # loop over all depters
-    i = 0
-    while i < len(data):
-        # configured the mail for a person
-        mails[i] = configure_massage.BildMail(data[i])
-        i += 1
-    log_writing.ProtSys('build mails from data')
-    #sending all mail to the server
-    if N >= int(config.settings['mail_quota']):
-        log_writing.ProtSys('mail_quota of {} is larger then Number of Mails {}'\
-            .format(config.settings['mail_quota'], N))
-        
-        N_days = math.ceil(N / int(config.settings['mail_quota']))
-        log_writing.ProtSys('The sending of the mails will be done over {} days'\
-            .format(math.ceil(N_days)))
-        
-        #Spliting mail list
-        daily_mails = list(accessories.divide_chunks(mails, int(config.settings['mail_quota'])))
-        
-        for dmails in daily_mails:
-            smtp_interactions.sendMails(dmails)
-            #the system waits a day
-            if dmails != daily_mails[-1]:
-                log_writing.ProtSys('Wait for tomorrow')
-                time.sleep(3600*24)
-    else:
-        log_writing.ProtSys('mails can all be send today')
-        #ProtSys('sending mail Decoie')
-        smtp_interactions.sendMails(mails)
-    log_writing.ProtSys('finished triggered operation')
-
-def MailToOne(reciver):
-    '''
-    Runs all necessary functions to build and send one mail to specified person.
-    '''
-    data = obtain_data.findDataForAddress(obtain_data.ReadTable(), reciver)
-    log_writing.ProtSys('got data of {}'.format(reciver))
-    mail = configure_massage.BildMail(data)
-    smtp_interactions.sendMails([mail])
+import combined_actions
 
 #_______________________________________________________________________________
 # Main function brings all together
@@ -77,9 +25,7 @@ def main():
     and repeadts the process.
     '''
     config.InitConfig()
-    
     smtp_interactions.testServerLogin()
-    
     validate_address.checkAll(obtain_data.ReadTable())
     
     print('start')

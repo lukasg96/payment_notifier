11cb3fb1f21b00d303c0511ab92595a4fd2a2ba8
lukas <lukas@grunwald-elzach.de>
2021-12-16 00:21:30 +0100

    2.4 satrted with style guide corrections

--- accessories.py
+++ accessories.py
@@ -1,5 +1,7 @@
-#_______________________________________________________________________________
+# ______________________________________________________________________________
 # Do all of the mail
-def divide_chunks(l, n):
-    for i in range(0, len(l), n): 
-        yield l[i:i + n]
+
+
+def divide_chunks(mails, n):
+    for i in range(0, len(mails), n):
+        yield mails[i:i + n]
--- combined_actions.py
+++ combined_actions.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Lukas Grunwald
 # Author: Lukas Grunwald <lukas@grunwald-elzach.de>
-#_______________________________________________________________________________
+# ______________________________________________________________________________
 import time
 
 import config
@@ -12,6 +12,8 @@ import smtp_interactions
 import imap_interactions
 import triggers
 import accessories
+# ______________________________________________________________________________
+
 
 def RunAllMails():
     '''
@@ -32,33 +34,41 @@ def RunAllMails():
         mails[i] = configure_massage.BildMail(data[i])
         i += 1
     log_writing.ProtSys('build mails from data')
-    #sending all mail to the server
+    # sending all mail to the server
     if N >= int(config.settings['mail_quota']):
-        log_writing.ProtSys('mail_quota of {} is larger then Number of Mails {}'\
-            .format(config.settings['mail_quota'], N))
-        
+        log_writing.ProtSys(
+            'mail_quota of {} is larger then Number of Mails {}'
+            .format(config.settings['mail_quota'], N)
+            )
+
         N_days = math.ceil(N / int(config.settings['mail_quota']))
-        log_writing.ProtSys('The sending of the mails will be done over {} days'\
-            .format(math.ceil(N_days)))
-        
-        #Spliting mail list
-        daily_mails = list(accessories.divide_chunks(mails, int(config.settings['mail_quota'])))
-        
+        log_writing.ProtSys(
+            'The sending of the mails will be done over {} days'
+            .format(math.ceil(N_days))
+            )
+
+        # Spliting mail list
+        daily_mails = list(accessories.divide_chunks(
+            mails, int(config.settings['mail_quota'])
+            ))
+
         for dmails in daily_mails:
             smtp_interactions.sendMails(dmails)
-            #the system waits a day
+            # the system waits a day
             if dmails != daily_mails[-1]:
                 log_writing.ProtSys('Wait for tomorrow')
                 time.sleep(3600*24)
     else:
         log_writing.ProtSys('mails can all be send today')
-        #ProtSys('sending mail Decoie')
+        # ProtSys('sending mail Decoie')
         smtp_interactions.sendMails(mails)
     log_writing.ProtSys('finished triggered operation')
 
+
 def MailToOne(reciver):
     '''
-    Runs all necessary functions to build and send one mail to specified person.
+    Runs all necessary functions to build and send one mail to
+    specified person.
     '''
     data = obtain_data.findDataForAddress(obtain_data.ReadTable(), reciver)
     log_writing.ProtSys('got data of {}'.format(reciver))
